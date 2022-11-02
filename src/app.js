6532b5ec965d322c7c6e108cc9189718708be7a4
rion <riiiiion@gmail.com>
2022-11-02 15:11:05 +0900

    一覧表示

--- src/app.js
+++ src/app.js
@@ -29,12 +29,31 @@ async function getNews(searchWord) {
   // .then((json) => console.log(json));
   // `https://newsapi.org/v2/top-headlines?country=jp&q='大谷'&apiKey=6b3b137d8d4148e788bfca5509e7069f`
   // https://newsapi.org/v2/everything?q=Ohtani&from=2022-11-01&sortBy=popularity&apiKey=6b3b137d8d4148e788bfca5509e7069f
-  console.log(result);
-  const body = document.getElementById("news");
+  //   console.log(result);
+  const newsList = document.getElementById("newsList");
   for (const elem of result) {
-    const newsTag = document.createElement("div");
-    newsTag.innerHTML = elem.title;
-    body.appendChild(newsTag);
+    const newsBlock = document.createElement("div");
+    const newsImage = document.createElement("img");
+    const titleAndContents = document.createElement("div");
+    const newsTitle = document.createElement("div");
+    const contents = document.createElement("div");
+    const newsLink = document.createElement("a");
+
+    newsLink.href = elem.url;
+    newsLink.classList.add("news_link");
+    newsImage.classList.add("news_image");
+    newsImage.src = elem.urlToImage;
+    newsTitle.innerHTML = elem.title;
+    contents.innerHTML = elem.description;
+
+    titleAndContents.appendChild(newsTitle);
+    titleAndContents.appendChild(contents);
+    newsBlock.appendChild(newsImage);
+    newsBlock.appendChild(titleAndContents);
+    newsBlock.classList.add("news_block");
+    newsLink.appendChild(newsBlock);
+
+    newsList.appendChild(newsLink);
   }
 }
 
