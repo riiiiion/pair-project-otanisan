9524dc8d9be4fec8c4582d2685a5e49e0ca49527
3181085 <hiroyuki_tsukiyama@mail.toyota.co.jp>
2022-11-02 16:18:20 +0900

    ootani 4

--- src/search.js
+++ src/search.js
@@ -1,24 +1,26 @@
 'use strict';
 
-
-
-window.keyOfSearch = '';
+//window.keyOfSearch = '';
 
 function no() {
     console.log("no");
-    window.keyOfSearch = '';
+    //window.keyOfSearch = '';
+    sessionStorage.removeItem('search_word');
     location.href = './display.html';
 }
 
 function yes() {
     console.log("yes");
-    window.keyOfSearch = '';
+    //window.keyOfSearch = '';
+    sessionStorage.removeItem('search_word');
     location.href = './search.html';
 }
 
 function search() {
+    console.log("search");
     const inputEl = document.getElementById('search_word');
-    window.keyOfSearch = inputEl.value;
+    //window.keyOfSearch = inputEl.value;
+    sessionStorage.setItem('search_word', inputEl.value);
     console.log(inputEl.value);
     location.href = 'display.html';
 }
